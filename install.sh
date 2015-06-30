558071c267e5eb4d5dc210fc8cba2ffbfe113dbe
romain.fontugne <romain.fontugne@gmail.com>
2015-06-30 15:53:39 +0900

    first version

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Installing vim plugins"
 vim +PluginInstall +qall
 
+echo "Install flake8"
+sudo pip install flake8
+
 echo "Compiling YCM"
 cd ~/.vim/bundle/YouCompleteMe; ./install.sh
 # cd ~/.vim/bundle/YouCompleteMe; ./install.sh --clang-completer
+
