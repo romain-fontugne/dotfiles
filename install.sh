e6f024cee5b8117ffe947139ddc871a4df6a2eb5
romain.fontugne <romain.fontugne@gmail.com>
2015-06-30 15:23:15 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing dotfiles"
+source install/link.sh
+
+echo "Installing vundle"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Installing vim plugins"
+vim +PluginInstall +qall
+
+echo "Compiling YCM"
+cd ~/.vim/bundle/YouCompleteMe; ./install.sh
+# cd ~/.vim/bundle/YouCompleteMe; ./install.sh --clang-completer
